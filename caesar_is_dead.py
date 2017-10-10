e1eaa3cb04e5fe5049ceecf2d7c66c5cbcf6bc35
Keoagile Dinake <kmdinake@gmail.com>
2017-10-10 23:59:00 +0200

    algorithm skeleton

---
+++ caesar_is_dead.py
@@ -0,0 +1,37 @@
+"""
+Using frequency analysis, attempt to decrypt any caesar's within well capabilities
+"""
+import sys
+import numpy as np
+
+
+def max_score_index(scores):
+    pass
+
+
+def find_shift(character, common_character_list):
+    pass
+
+
+def get_frequency(character, cipher_text):
+    pass
+
+
+def get_plain_text(shift, cipher_text):
+    pass
+
+
+def decrypt(cipher_text, character_set):
+    pass
+
+
+def main():
+    # read in the encrypted text
+    cipher_text = ""
+    character_set = ""
+    # read in the character set
+    decrypt(cipher_text, character_set)
+
+
+if __name__ == '__main__':
+    main()
